68687c96a26ccaef0dcd1910a311cfec70df4418
Dion Johnson <dionjwa@gmail.com>
2021-04-20 22:43:36 -0700

    completed all 4 cc 06

--- code-challenges/challenges-06.test.js
+++ code-challenges/challenges-06.test.js
@@ -30,7 +30,7 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 ------------------------------------------------------------------------------------------------ */
 
-const appendTheEnd = (str) => str + 'The End.';
+const appendTheEnd = (str) => str + ' The end.';
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
