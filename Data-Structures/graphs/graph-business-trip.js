5772d9924a6007b3ef3203d2be341ebf9b500041
dionjwa1 <dionjwa@gmail.com>
2021-08-03 19:23:56 -0700

    cc37

---
+++ Data-Structures/graphs/graph-business-trip.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let arr = ['Pandora', 'Arendelle', 'Metroville', 'Monstropolis', 'Narnia', 'Naboo'];
+
+const businessTrip = (graph, cityNames) => {
+   let graphAdj: $82,$115, $0
+   let arr:[graphAdj:$82(Metroville, Pandora)],
+    return()
+}
+const myGraph = (node) => {
+    visited[node] = true;
+    while (!node.isEmpty(node.next)) {
+        let visiting = node.next();
+        console.log(`been here...done that ${visiting}`);
+        for (let j = 0; j < graphAdj[visiting].length; j++) {
+            if ((graphAdj[visiting][j] === [arr]) && (visited[j] === false)) {
+                visited[j] = true;
+                console.log(visiting);
+            }
+        }
+    }
+}
+
+myGraph();
